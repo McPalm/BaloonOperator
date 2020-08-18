32e2bd555454a6492d1286cd5d32ef9b5514eb3f
palm <tauman.exe@gmail.com>
2020-08-18 13:49:56 +0200

    Added health, untested. But not implemented on anything outside of the terraintest yet.

---
+++ Assets/Code/Combat/Health.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
+
+public class Health : MonoBehaviour
+{
+    public int MaxHealth = 1;
+
+    public int HealthLost { get; set; }
+    public int CurrentHealth => MaxHealth - HealthLost;
+
+    public event System.Action<int, int> OnChange;
+    public event System.Action<int> OnHurt;
+    public event System.Action<int> OnHeal;
+    public event System.Action OnZeroHealth;
+
+    public UnityEvent<float> OnChangeHealth;
+
+    private void Start()
+    {
+        OnChangeHealth.Invoke((float)CurrentHealth / (float)MaxHealth);
+    }
+
+    public void Hurt(int damage)
+    {
+        if (damage < 1)
+            return;
+        HealthLost -= damage;
+        OnHurt(damage);
+        OnChange?.Invoke(CurrentHealth, MaxHealth);
+        if (HealthLost >= MaxHealth)
+            OnZeroHealth?.Invoke();
+        OnChangeHealth.Invoke(CurrentHealth);
+    }
+
+    public void Heal(int damage)
+    {
+        OnHeal(damage);
+        if (damage > HealthLost)
+            damage = HealthLost;
+        if (damage < 1)
+            return;
+        OnChange?.Invoke(CurrentHealth, MaxHealth);
+        OnChangeHealth.Invoke(CurrentHealth);
+    }
+
+    public void SetHealth(int lost, int change)
+    {
+        HealthLost = lost;
+        OnChangeHealth.Invoke((float)CurrentHealth / (float) MaxHealth);
+    }
+}
