18012d687cedb0ce621661d087acd3100dcdd3d1
palm <tauman.exe@gmail.com>
2020-08-18 14:15:17 +0200

    Added missing network identity to the SceneLoader

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -21,12 +21,6 @@ public class GameManager : NetworkBehaviour
         ///Move character to the right position
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ResetScene()
     {
         SceneLoader.ReloadScene();
