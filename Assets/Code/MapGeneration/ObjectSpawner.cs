21abd52eedd308a69c1d381662a9696023f8b6a7
palm <tauman.exe@gmail.com>
2020-08-21 15:12:39 +0200

    Spawn in Enemies as a part of the Modules.

---
+++ Assets/Code/MapGeneration/ObjectSpawner.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject GameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var obj = Instantiate(GameObject, transform.position, Quaternion.identity);
+        obj.transform.parent = transform;
+        obj.transform.parent = null;
+        GetComponent<SpriteRenderer>().enabled = false;
+        NetworkServer.Spawn(obj);
+    }
+
+}
