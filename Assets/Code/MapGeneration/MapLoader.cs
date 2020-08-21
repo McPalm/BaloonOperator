fbd5c26fa7df465b9e73e455c60574c19a037298
Peter Åkesson <petak86@gmail.com>
2020-08-21 16:42:45 +0200

    Added networkmessages for generated levels.

--- Assets/Code/MapGeneration/MapLoader.cs
+++ Assets/Code/MapGeneration/MapLoader.cs
@@ -1,46 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using System.Runtime.CompilerServices;
 
-public class MapLoader : MonoBehaviour
+public class MapLoader : NetworkBehaviour
 {
+    struct SerializedMapModule
+    {
+        public int index;
+        public bool flip;
+    }
+
 
     public MapModuleSample[] mapModuleSamples;
     public MapModuleSet[] mapModuleSets;
 
     public MapPainter MapPainter;
 
+    List<MapModuleSample> modules = new List<MapModuleSample>();
+
     MapGenerator generator;
 
+    private void Awake()
+    {
+        modules.AddRange(mapModuleSamples);
+        foreach (var set in mapModuleSets)
+            modules.AddRange(set.MapModules);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        List<MapModuleSample> modules = new List<MapModuleSample>();
-        modules.AddRange(mapModuleSamples);
-        foreach (var set in mapModuleSets)
-            modules.AddRange(set.MapModules);
-        generator = new MapGenerator()
+        if (isServer)
         {
-            modules = modules.ToArray()
-        };
-        MapModule[] generatedMap = null;
-        for(int i = 0; i < 100; i++)
-        {
-            try
+            generator = new MapGenerator()
             {
-                generatedMap = generator.GenerateMap();
-                break;
-            }
-            catch
+                modules = modules.ToArray()
+            };
+            MapModule[] generatedMap = null;
+            for (int i = 0; i < 100; i++)
             {
-                Debug.Log($"Failed Attempt {i}");
-            }
+                try
+                {
+                    generatedMap = generator.GenerateMap();
+                    break;
+                }
+                catch
+                {
+                    Debug.Log($"Failed Attempt {i}");
+                }
 
+            }
+            SerializedMapModule[] serializedArray = new SerializedMapModule[16];
+            for(int i = 0; i < 16; i++)
+            {
+                serializedArray[i].index = modules.IndexOf(generatedMap[i].MapModuleSample);
+                serializedArray[i].flip = generatedMap[i].flip;
+            }
+            MapPainter.Paint(generatedMap);
+            RpcPaintMap(serializedArray);
         }
-
-        MapPainter.Paint(generatedMap);
 
     }
 
+    [ClientRpc(channel = Channels.DefaultReliable)]
+    private void RpcPaintMap(SerializedMapModule[] serializedMap)
+    {
+        MapModule[] generatedMap = new MapModule[16];
+        for (int i = 0; i < 16; i++)
+        {
+            generatedMap[i] = new MapModule(modules[serializedMap[i].index], serializedMap[i].flip);
+        }
+        MapPainter.Paint(generatedMap);
+    }
 }
