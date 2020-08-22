c5a0469e004f6ba077b2f72ff3d4ec5d36632f7c
palm <tauman.exe@gmail.com>
2020-08-22 23:01:53 +0200

    Added a script for enemies to perofrm attacks, network synced and everything. I even wrote comments on the functions!

---
+++ Assets/Code/Combat/EnemyAttack.cs
@@ -0,0 +1,38 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : NetworkBehaviour
+{
+    /// <summary>
+    ///  perform an attack, network synced
+    /// </summary>
+    public void Attack() => RpcAttack();
+    /// <summary>
+    ///  perform an attack with a delay, will ensure it happens simultaneously on different clients, and with some lag protection, if you want consequetive syncronized attacks I suppose.
+    /// </summary>
+    /// <param name="delay"></param>
+    public void AttackIn(float delay) => RpcAttackIn(NetworkTime.time + delay);
+
+    public Animator Animator;
+
+    [ClientRpc(channel = Channels.DefaultReliable)]
+    void RpcAttack()
+    {
+        Animator.SetTrigger("Strike");
+    }
+
+    [ClientRpc(channel = Channels.DefaultReliable)]
+    void RpcAttackIn(double time)
+    {
+        float delay = (float)(time - NetworkTime.time);
+        StartCoroutine(AttackInCoroutine(delay));
+    }
+
+    IEnumerator AttackInCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Animator.SetTrigger("Strike");
+    }
+}
