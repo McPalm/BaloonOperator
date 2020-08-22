b5c95c43c08c420291dc98cdf660df8e63c0bd7a
palm <tauman.exe@gmail.com>
2020-08-22 11:14:17 +0200

    spikes dont hurt you while sliding or climbing.

--- Assets/Code/Combat/SpikesHurtYou.cs
+++ Assets/Code/Combat/SpikesHurtYou.cs
@@ -8,13 +8,22 @@ public class SpikesHurtYou : MonoBehaviour
     {
         var mobile = collision.GetComponent<Mobile>();
         var health = collision.GetComponent<Health>();
+        var player = collision.GetComponent<PlatformingCharacter>();
         if (mobile && health)
         {
-            if (mobile.VMomentum < -16f)
+            if(player)
+            {
+                if (player.Climbing)
+                    return;
+                if(player.WallSliding)
+                return;
+            }
+
+            if (mobile.VMomentum < -18f)
                 Hurt(health, 3);
-            else if (mobile.VMomentum < -8f)
+            else if (mobile.VMomentum < -9f)
                 Hurt(health, 2);
-            else if (mobile.VMomentum < -3f)
+            else if (mobile.VMomentum < -4f)
                 Hurt(health, 1);
 
         }
