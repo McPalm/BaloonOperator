0ceaa753da464c01749499c6bba9fe617dbe42df
palm <tauman.exe@gmail.com>
2020-08-24 18:56:25 +0200

    Added a hammer, enabled as default for now. But should be a pickup.

---
+++ Assets/Code/Combat/AnimationPhysics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPhysics : MonoBehaviour
+{
+    public PlatformingCharacter player;
+    public Mobile mobile;
+
+    public float xForce;
+    public float yForce;
+
+    public bool rooted;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(player)
+        {
+            if (rooted)
+                player.Root(2);
+        }
+        if(mobile)
+        {
+            if (xForce != 0f)
+                mobile.HMomentum = xForce * mobile.Forward;
+            if (yForce != 0f)
+                mobile.VMomentum = yForce;
+        }
+    }
+}
