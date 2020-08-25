235a862c12af30c575d563615dd0effead9a850c
palm <tauman.exe@gmail.com>
2020-08-25 19:24:28 +0200

    Polished the nuts out of attack animations and what not.

---
+++ Assets/Code/Combat/AttackAnimationProperties.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimationProperties : MonoBehaviour
+{
+    public PlatformingCharacter Player;
+    public Animator Animator;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Animator.SetBool("Grounded", Player.Grounded);
+    }
+}
