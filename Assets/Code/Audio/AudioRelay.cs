d3f64fba9806f64ed818241e1b0f84ec725bdda6
palm <tauman.exe@gmail.com>
2020-08-25 23:15:44 +0200

    Added some basic audio to combat.

---
+++ Assets/Code/Audio/AudioRelay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRelay : MonoBehaviour
+{
+    public AudioClip clip;
+    public float volume = 0f;
+
+    public void PlaySound()
+    {
+        AudioPool.PlaySound(transform.position, clip, volume);
+    }
+}
--- Assets/Code/Combat/ContactDamage.cs
+++ Assets/Code/Combat/ContactDamage.cs
@@ -7,17 +7,24 @@ public class ContactDamage : MonoBehaviour
     public GameObject source;
     public int damage = 1;
 
+    public AudioClip OnHitSound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!enabled)
             return;
         var health = collision.GetComponent<Health>();
         if (health)
+        {
+
             health.Hurt(new DamageData()
             {
                 damage = damage,
                 source = source == null ? gameObject : source,
             });
+            if(OnHitSound != null)
+                AudioPool.PlaySound(clip: OnHitSound, position: transform.position);
+        }
     }
     
 }
