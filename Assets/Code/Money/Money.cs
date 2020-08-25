d2b7d5a05b109770f1cbf0ac5131fb2f806476b8
palm <tauman.exe@gmail.com>
2020-08-25 23:22:53 +0200

    More sound effects. (picking up valuables, hammer swing swosh, stage win)

--- Assets/Code/Money/Money.cs
+++ Assets/Code/Money/Money.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Money : MonoBehaviour
 {
     public int value = 10;
+    public AudioClip clip;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioPool.PlaySound(transform.position, clip, 1f);
         FindObjectOfType<Wallet>().AddMoney(value);
         Destroy(gameObject);
         NetworkServer.Destroy(gameObject);
