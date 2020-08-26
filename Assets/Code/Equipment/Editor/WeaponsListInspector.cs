f63132441c2363dd97ee5eb8f5b7b816544b807c
palm <tauman.exe@gmail.com>
2020-08-26 23:32:59 +0200

    Indexing weapons.

---
+++ Assets/Code/Equipment/Editor/WeaponsListInspector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WeaponsList))]
+public class WeaponsListInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Generate"))
+        {
+            var list = (WeaponsList)target;
+            list.WeaponProperties = GetAll();
+            EditorUtility.SetDirty(list);
+        }
+
+        DrawDefaultInspector();
+    }
+
+
+    WeaponProperties[] GetAll()
+    {
+
+        string[] guids = AssetDatabase.FindAssets("t:" + typeof(WeaponProperties).Name);  //FindAssets uses tags check documentation for more info
+        WeaponProperties[] a = new WeaponProperties[guids.Length];
+        for (int i = 0; i < guids.Length; i++)         //probably could get optimized 
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            a[i] = AssetDatabase.LoadAssetAtPath<WeaponProperties>(path);
+        }
+        return a;
+    }
+}
---
+++ Assets/Code/Equipment/WeaponsList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "WeaponsList", menuName = "WeaponsList", order = 123451)]
+public class WeaponsList : ScriptableObject
+{
+    public WeaponProperties[] WeaponProperties;
+
+    
+}
