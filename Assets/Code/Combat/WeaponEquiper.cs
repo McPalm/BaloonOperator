d996f331a3a9a09fa36934e2ee1fe1c75e11d711
palm <tauman.exe@gmail.com>
2020-08-26 22:35:29 +0200

    Weapon properites as scriptable object that can be equipped.

---
+++ Assets/Code/Combat/WeaponEquiper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponEquiper : MonoBehaviour
+{
+    [SerializeField] WeaponProperties equipped;
+
+    public SpriteRenderer spriteRenderer;
+    public Transform swipeTrail;
+    public ContactDamage ContactDamage;
+    public Animator Animator;
+    public PolygonCollider2D Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Equip(equipped);
+    }
+
+
+    public void Equip(WeaponProperties weapon)
+    {
+        spriteRenderer.sprite = weapon.sprite;
+        swipeTrail.localPosition = new Vector3(weapon.lenght, 0f);
+        ContactDamage.damage = weapon.damage;
+        Animator.SetFloat("AttackSpeed", weapon.attackSpeed);
+        Animator.SetBool("Hammer", weapon.hammer);
+        List<Vector2> points = new List<Vector2>();
+        weapon.sprite.GetPhysicsShape(0, points);
+        Collider.SetPath(0, points);
+    }
+
+    
+}
---
+++ Assets/Code/Combat/WeaponProperties.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon Properties", menuName = "Weapon Properties")]
+public class WeaponProperties : ScriptableObject
+{
+    public Sprite sprite;
+    public float lenght = .25f;
+    public int damage = 1;
+    public float attackSpeed = 1f;
+    public bool hammer;
+    public bool breaksBlocks;
+}
