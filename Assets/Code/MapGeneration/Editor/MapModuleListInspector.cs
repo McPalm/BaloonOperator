88d94158e67466f73a64d23d6008034003f12fe0
palm <tauman.exe@gmail.com>
2020-08-29 14:44:25 +0200

    A scriptable object to index all maps modules.

---
+++ Assets/Code/MapGeneration/Editor/MapModuleListInspector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapModuleList))]
+public class MapModuleListInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Generate"))
+        {
+            var list = (MapModuleList)target;
+            list.Samples = new List<MapModuleSample>(GetAll());
+            EditorUtility.SetDirty(list);
+        }
+
+        DrawDefaultInspector();
+    }
+
+
+    MapModuleSample[] GetAll()
+    {
+
+        string[] guids = AssetDatabase.FindAssets("t:" + typeof(MapModuleSample).Name);  //FindAssets uses tags check documentation for more info
+        MapModuleSample[] a = new MapModuleSample[guids.Length];
+        for (int i = 0; i < guids.Length; i++)         //probably could get optimized 
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guids[i]);
+            a[i] = AssetDatabase.LoadAssetAtPath<MapModuleSample>(path);
+        }
+        return a;
+    }
+}
---
+++ Assets/Code/MapGeneration/MapModuleList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MapModuleList : ScriptableObject
+{
+    public List<MapModuleSample> Samples;
+
+    public MapModuleSample GetByIndex(int index) => Samples[index];
+
+    public int GetIndexFor(MapModuleSample sample) => Samples.IndexOf(sample);
+        
+         
+}
