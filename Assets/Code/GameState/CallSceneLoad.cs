d7bba9e3115cf23bd39f450117a4e91460df0f13
palm <tauman.exe@gmail.com>
2020-09-03 19:06:22 +0200

    Tutorial quit out to the main menu.

---
+++ Assets/Code/GameState/CallSceneLoad.cs
@@ -0,0 +1,29 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallSceneLoad : MonoBehaviour
+{
+    [Scene]
+    public string SceneToLoad;
+
+
+    public float autoFireInSeconds = 0f;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        if(autoFireInSeconds > 0f)
+        {
+            yield return null;
+            yield return new WaitForSeconds(autoFireInSeconds);
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        var loader = FindObjectOfType<SceneLoader>();
+        loader.LoadScene(SceneToLoad);
+    }
+}
---
+++ Assets/Code/GameState/OnlineSessionEnder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlineSessionEnder : MonoBehaviour
+{
+    public float EndSessionInSeconds = 0f;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        if(EndSessionInSeconds > 0f)
+        {
+            yield return null;
+            yield return new WaitForSeconds(EndSessionInSeconds);
+            EndSession();
+        }
+    }
+
+   public void EndSession()
+    {
+        MyNetworkManager.singleton.StopClient();
+        if(MyNetworkManager.isServer)
+            MyNetworkManager.singleton.StopHost();
+    }
+}
